11c1ecd36c21a22e766b39c1fa080be07470459b
Panachai <panachai.ny@gmail.com>
2024-08-14 16:53:48 +0700

    1

--- convert_csv.test.ts
+++ convert_csv.test.ts
@@ -1,21 +1,51 @@
 import { expect, test } from "bun:test";
-import { convertCsv, type CsvARow } from "./convert_csv";
+import { convertCsv, type CsvBRow } from "./convert_csv";
 import * as fs from 'fs';
-import { parse } from "fast-csv";
+import * as csv from 'fast-csv';
+// import * as csv from '@fast-csv/parse';
 
-test("when convert csv_a to csv_b should be correct", () => {
+// test("when convert csv_a to csv_b should be correct", () => {
+//     const inputPath = 'assets/templates/source_tasks.csv'
+//     const outputPath = 'des_import_test.csv'
+
+//     convertCsv(inputPath, outputPath);
+
+//     const csvData: CsvBRow[] = [];
+
+//     // Read CSV_A
+//     fs.createReadStream(outputPath)
+//         .pipe(parse())
+//         .on('data', (data: CsvBRow) => csvData.push(data))
+
+//     // console.log(csvAData)
+
+//     expect(csvData).toEqual([""])
+// });
+
+
+
+
+
+async function readCsv(filePath: string): Promise<any[]> {
+    const data: any[] = [];
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(filePath)
+            .pipe(csv.parse())
+            .on('data', (row) => data.push(row))
+            .on('end', () => resolve(data))
+            .on('error', (error) => reject(error));
+    });
+}
+
+
+test("2222222", async () => {
     const inputPath = 'assets/templates/source_tasks.csv'
     const outputPath = 'des_import_test.csv'
-    convertCsv(inputPath, outputPath);
 
-    const csvAData: CsvARow[] = [];
+    // convertCsv(inputPath, outputPath);
 
-    // Read CSV_A
-    fs.createReadStream(outputPath)
-        .pipe(parse())
-        .on('data', (data: CsvARow) => csvAData.push(data))
-
-    console.log(csvAData)
-
-    expect(csvAData).toEqual([])
+    var actualData = await readCsv(outputPath)
+        .then(data => data)
+        .catch(err => console.error(err));
+    expect(actualData).toEqual([""])
 });
