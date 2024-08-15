b21e98d33efb76d804b4d6a7dc8b1efef145f028
panachainy <panachai.ny@gmail.com>
2024-08-16 00:54:12 +0700

    update

--- v/csv-estimate-to-csv-import-jira-web/src/main.tsx
+++ v/csv-estimate-to-csv-import-jira-web/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Container } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Container maxWidth="sm">
+      <App />
+    </Container>
   </StrictMode>,
 )
