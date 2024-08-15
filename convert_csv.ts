71e75a082cca894c288801ab5cb921ae3d4c2933
Panachai <panachai.ny@gmail.com>
2024-08-15 12:57:05 +0700

    clean

--- convert_csv.ts
+++ convert_csv.ts
@@ -25,17 +25,11 @@ async function convertCsv(inputFilePath: string, outputFilePath: string): Promis
     const exitingEpic: string[] = [];
     const exitingStory: string[] = [];
 
-    // FIXME: https://c2fo.github.io/fast-csv/docs/introduction/example
     // Read CSV_A
     return new Promise((resolve, reject) => {
         fs.createReadStream(inputFilePath)
             .pipe(csv.parse({ headers: true }))
-            // .pipe(
-            //     csv.format<CsvARow, CsvBRow>({ headers: true })
-            // )
             .on('data', (row: any) => {
-                // console.log(row)
-                // check if epic
                 if (!exitingEpic.includes(row.Epic)) {
 
                     csvData.push({
@@ -46,15 +40,6 @@ async function convertCsv(inputFilePath: string, outputFilePath: string): Promis
                         Parent: "",
                     } as CsvBRow)
                     exitingEpic.push(row.Epic);
-
-                    // // FIXME: transform only epic
-                    // return next(null, {
-                    //     IssueType: "Epic",
-                    //     Summary: row.Epic,
-                    //     // 'Story point': 0,
-                    //     IssueKey: row.Epic,
-                    //     // Parent: "",
-                    // } as CsvBRow);
                 }
 
                 if (!exitingStory.includes(row["Features (Story)"])) {
@@ -69,24 +54,11 @@ async function convertCsv(inputFilePath: string, outputFilePath: string): Promis
                     exitingStory.push(row["Features (Story)"]);
                 }
 
-                // console.log('transform', row);
-
-                // return next(null, {
-                //     IssueType: "e",
-                //     Summary: "",
-                //     'Story point': 1,
-                //     IssueKey: "",
-                //     Parent: "",
-                // } as CsvBRow);
-
 
 
                 const sp = row['DevOps (MD)'] as number + row['BE (MD)'] as number + row['FE (MD)'] as number + row['UX UI (MD)'] as number + row['QA (MD)'] as number
 
 
-                // Epic: string;
-                // 'Features (Story)': string;
-                // 'Sub-Task': string;
 
                 csvData.push({
                     IssueType: "Subtask",
@@ -96,140 +68,22 @@ async function convertCsv(inputFilePath: string, outputFilePath: string): Promis
                     Parent: row['Features (Story)'],
                 } as CsvBRow)
             })
-            // .transform((row, next): void => {
-            //     console.log(row)
-            //     // check if epic
-            //     if (!exitingEpic.includes(row.Epic)) {
-            //         exitingEpic.push(row.Epic);
 
-            //         // FIXME: transform only epic
-            //         return next(null, {
-            //             IssueType: "Epic",
-            //             Summary: row.Epic,
-            //             // 'Story point': 0,
-            //             IssueKey: row.Epic,
-            //             // Parent: "",
-            //         } as CsvBRow);
-            //     }
-
-            //     if (!exitingStory.includes(row["Features (Story)"])) {
-            //         exitingStory.push(row["Features (Story)"]);
-
-            //         // FIXME: transform only epic
-            //         return;
-            //     }
-
-            //     // console.log('transform', row);
-
-            //     return next(null, {
-            //         IssueType: "e",
-            //         Summary: "",
-            //         'Story point': 1,
-            //         IssueKey: "",
-            //         Parent: "",
-            //     } as CsvBRow);
-            // })
-            // .pipe(process.stdout)
             .on('end', (row: any) => {
                 csv.write(csvData, { headers: true })
                     .pipe(fs.createWriteStream(outputFilePath))
                     .on('finish', () => {
                         console.log('CSV_B file created successfully');
 
-                        // Replace with your Jira API endpoint and authentication
-                        // const jiraApi = axios.create({
-                        //     baseURL: jiraUrl,
-                        //     headers: {
-                        //         Authorization: `Basic ${Buffer.from(`${jiraToken}:`).toString('base64')}`,
-                        //         'Content-Type': 'application/json',
-                        //     },
-                        // });
 
-                        // // Example of creating an issue in Jira
-                        // const createIssue = async (issueData: CSV_B_Row) => {
-                        //     try {
-                        //         const response = await jiraApi.post('/rest/api/3/issue', issueData);
-                        //         console.log('Issue created:', response.data);
-                        //     } catch (error) {
-                        //         console.error('Error creating issue:', error);
-                        //     }
-                        // };
-
-                        // outputData.forEach(async (issue) => {
-                        //     await createIssue(issue);
-                        // });
-
-                        // resolve();
                     });
 
                 resolve();
-                // console.log(row);
             })
             .on('error', (error) => reject(error));
     });
 
 
-    // .on('data', (data: CsvARow) => csvAData.push(data))
-    // .on('end', () => {
-    //     console.log(csvAData)
-
-    // })
-    // .on('end', () => {
-    //     // Data transformation logic
-    //     const csvBData: CsvBRow[] = [];
-
-    //     // Basic transformation example - replace with your actual logic
-    //     csvAData.forEach((row, index) => {
-    //         const issueType = row['Sub-Task'] ? 'Sub-Task' : row['Features (Story)'] ? 'Story' : 'Epic';
-    //         const storyPoint = row['DevOps (MD)'] + row['BE (MD)'] + row['FE (MD)'] + row['UX UI (MD)'] + row['QA (MD)'];
-    //         const issueKey = row['Sub-Task'] || row['Features (Story)'] || row.Epic; // Replace with a more robust logic
-    //         const parent = issueType === 'Sub-Task' ? row['Features (Story)'] : issueType === 'Story' ? row.Epic : '';
-
-    //         // Note: skip header
-    //         if (index == 0) { return }
-
-    //         csvBData.push({
-    //             IssueType: issueType,
-    //             Summary: row[issueType],
-    //             'Story point': storyPoint,
-    //             IssueKey: issueKey,
-    //             Parent: parent,
-    //         });
-    //     });
-
-    //     fastCsv
-    //         .write(csvBData, { headers: true })
-    //         .pipe(fs.createWriteStream(outputFilePath))
-    //         .on('finish', () => {
-    //             console.log('CSV_B file created successfully');
-
-
-    //             // Replace with your Jira API endpoint and authentication
-    //             // const jiraApi = axios.create({
-    //             //     baseURL: jiraUrl,
-    //             //     headers: {
-    //             //         Authorization: `Basic ${Buffer.from(`${jiraToken}:`).toString('base64')}`,
-    //             //         'Content-Type': 'application/json',
-    //             //     },
-    //             // });
-
-    //             // // Example of creating an issue in Jira
-    //             // const createIssue = async (issueData: CSV_B_Row) => {
-    //             //     try {
-    //             //         const response = await jiraApi.post('/rest/api/3/issue', issueData);
-    //             //         console.log('Issue created:', response.data);
-    //             //     } catch (error) {
-    //             //         console.error('Error creating issue:', error);
-    //             //     }
-    //             // };
-
-    //             // outputData.forEach(async (issue) => {
-    //             //     await createIssue(issue);
-    //             // });
-
-    //             // resolve();
-    //         });
-    // });
 }
 
 export { convertCsv };
--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
 import { convertCsv } from "./convert_csv";
 
-convertCsv('assets/templates/source_tasks.csv', 'assets/templates/des_import.csv');
+const inputPath = 'assets/templates/source_tasks.csv'
+const outputPath = 'assets/templates/des_import.csv'
+
+await convertCsv(inputPath, outputPath);
+
+console.log("convertCsv done")
