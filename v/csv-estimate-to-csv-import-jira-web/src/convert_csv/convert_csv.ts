700a2f9042633218ba249c0d01690fb377558752
Panachai <panachai.ny@gmail.com>
2024-08-16 10:32:51 +0700

    add description

--- v/csv-estimate-to-csv-import-jira-web/src/convert_csv/convert_csv.ts
+++ v/csv-estimate-to-csv-import-jira-web/src/convert_csv/convert_csv.ts
@@ -5,6 +5,7 @@ interface CsvARow {
     Epic: string;
     'Features (Story)': string;
     'Sub-Task': string;
+    Description: string;
     'DevOps (MD)': string;
     'BE (MD)': string;
     'FE (MD)': string;
@@ -15,6 +16,7 @@ interface CsvARow {
 interface CsvBRow {
     IssueType: string;
     Summary: string;
+    Description: string;
     'Story point': string;
     IssueKey: string;
     Parent: string;
@@ -47,22 +49,19 @@ async function convertCsv(inputFilePath: string, outputFilePath: string): Promis
                     csvData.push({
                         IssueType: "Story",
                         Summary: row['Features (Story)'],
+                        Description: 'wait to add by PO',
                         'Story point': "",
                         IssueKey: row['Features (Story)'],
                         Parent: row.Epic,
                     } as CsvBRow)
                     exitingStory.push(row["Features (Story)"]);
                 }
-
-
-
                 const sp = row['DevOps (MD)'] as number + row['BE (MD)'] as number + row['FE (MD)'] as number + row['UX UI (MD)'] as number + row['QA (MD)'] as number
-
-
 
                 csvData.push({
                     IssueType: "Subtask",
                     Summary: row['Sub-Task'],
+                    Description: row.Description,
                     'Story point': sp + "",
                     IssueKey: row['Sub-Task'],
                     Parent: row['Features (Story)'],
