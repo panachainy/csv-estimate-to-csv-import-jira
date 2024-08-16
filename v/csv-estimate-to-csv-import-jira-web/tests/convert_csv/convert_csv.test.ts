9de3ba01a90f89185baa304e68e8a8b59a972dd3
Panachai <panachai.ny@gmail.com>
2024-08-16 10:29:40 +0700

    update test web

--- v/csv-estimate-to-csv-import-jira-web/tests/convert_csv/convert_csv.test.ts
+++ v/csv-estimate-to-csv-import-jira-web/tests/convert_csv/convert_csv.test.ts
@@ -1,11 +1,11 @@
 import { expect, test } from "bun:test";
-import { convertCsv } from "@/src/convert_csv/convert_csv";
+import { convertCsv } from "../../src/convert_csv/convert_csv";
 import * as fs from 'fs';
 
 test("when convertCsv should be correct expect data", async () => {
-    const inputPath = 'assets/templates/source_tasks.csv'
+    const inputPath = 'tests/convert_csv/source_tasks.csv'
     const outputPath = './des_import_test.csv'
-    const expectPath = './tests/expect_csv.csv'
+    const expectPath = 'tests/convert_csv/expect_csv.csv'
 
     await convertCsv(inputPath, outputPath);
 
